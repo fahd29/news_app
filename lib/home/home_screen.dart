cf3bfcfda7893dd4966356090ce57047c961ef03
FAHD FAYED <fahdfayed6@gmail.com>
2024-08-29 09:32:21 +0300

    initial project added empty home screen

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreeen extends StatelessWidget {
+
+static const String routeName='home_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
